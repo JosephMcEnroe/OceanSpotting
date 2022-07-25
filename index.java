e6b00fcabddb0c3d5bc5a923c07aa110e7bd9f2a
JosephMcE890 <jcm1664@rit.edu>
2022-07-25 15:24:43 -0400

    2nd Update

--- index.java
+++ index.java
@@ -1,12 +1,21 @@
 public class index {
     public static void main(String[] args)
     {
+        Title();        // Title
         Menu();
     }
     
+    public static void Title()
+    {
+        System.out.println("\t\t\t---SEA ANIMAL SPOTTING---");
+        System.out.println("\t\t\t   By: Joseph McEnroe");
+    }
     public static void Menu()
     {
-        System.out.println("\t\t\t---SEA ANIMAL SPOTTING---\n");
-        System.out.println("\t-OPTIONS-");
+        System.out.println("\n\t\t\t       -OPTIONS-");
+
+        System.out.println("\n\t1) Enter Data");
+        System.out.println("\t2) Display All Data");
+
     }
 }
