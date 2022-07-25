73e57b648169122339ca74aceb231551e9751964
JosephMcE890 <jcm1664@rit.edu>
2022-07-25 00:02:11 -0400

    First Update!

---
+++ index.java
@@ -0,0 +1,12 @@
+public class index {
+    public static void main(String[] args)
+    {
+        Menu();
+    }
+    
+    public static void Menu()
+    {
+        System.out.println("\t\t\t---SEA ANIMAL SPOTTING---\n");
+        System.out.println("\t-OPTIONS-");
+    }
+}
